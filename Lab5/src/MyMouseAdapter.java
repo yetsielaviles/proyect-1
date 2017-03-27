008a6e502934564c34691871ec68159d50fac064
yetsiel.aviles <yetsiel.aviles@INCADELWS14.ecenet.ece.uprm.edu>
2017-03-27 18:56:22 -0400

    Minesweeper almost finished. Still needs the game over or you won messages and still needs some improvement.

---
+++ Lab5/src/MyMouseAdapter.java
@@ -0,0 +1,303 @@
+import java.awt.Color;
+
+import java.awt.Component;
+
+import java.awt.Insets;
+
+import java.awt.event.MouseAdapter;
+
+import java.awt.event.MouseEvent;
+
+
+import javax.swing.JFrame;
+
+
+public class MyMouseAdapter extends MouseAdapter {
+
+   
+    
+    public void mousePressed(MouseEvent e) {
+
+        switch (e.getButton()) {
+
+        case 1:    //Left mouse button
+
+            Component c = e.getComponent();
+
+            while (!(c instanceof JFrame)) {
+
+                c = c.getParent();
+
+                if (c == null) {
+
+                    return;
+
+                }
+
+            }
+
+            JFrame myFrame = (JFrame) c;
+
+            MyPanel myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);
+
+            Insets myInsets = myFrame.getInsets();
+
+            int x1 = myInsets.left;
+
+            int y1 = myInsets.top;
+
+            e.translatePoint(-x1, -y1);
+
+            int x = e.getX();
+
+            int y = e.getY();
+
+            myPanel.x = x;
+
+            myPanel.y = y;
+
+            myPanel.mouseDownGridX = myPanel.getGridX(x, y);
+
+            myPanel.mouseDownGridY = myPanel.getGridY(x, y);
+
+            myPanel.repaint();
+          
+         
+              
+            break;
+            
+      
+
+
+
+        case 3:    //Right mouse button
+
+            //Do nothing
+            Component c2 = e.getComponent();
+
+            while (!(c2 instanceof JFrame)) {
+
+                c2 = c2.getParent();
+
+                if (c2 == null) {
+
+                    return;
+
+                }
+
+            }
+            JFrame myFrame2 = (JFrame) c2;
+
+            MyPanel myPanel2 = (MyPanel) myFrame2.getContentPane().getComponent(0);
+
+            Insets myInsets2 = myFrame2.getInsets();
+
+            int x3 = myInsets2.left;
+
+            int y3 = myInsets2.top;
+
+            e.translatePoint(-x3, -y3);
+
+            int x2 = e.getX();
+
+            int y2 = e.getY();
+
+            myPanel2.x = x2;
+
+            myPanel2.y = y2;
+
+            myPanel2.mouseDownGridX = myPanel2.getGridX(x2, y2);
+
+            myPanel2.mouseDownGridY = myPanel2.getGridY(x2, y2);
+
+
+
+            myPanel2.repaint();
+
+            break;
+
+
+
+
+        default:    //Some other button (2 = Middle mouse button, etc.)
+
+            //Do nothing
+
+            break;
+
+        }
+
+    }
+
+    public void mouseReleased(MouseEvent e) {
+
+        switch (e.getButton()) {
+
+        case 1:    //Left mouse button
+
+            Component c = e.getComponent();
+
+            while (!(c instanceof JFrame)) {
+
+                c = c.getParent();
+
+                if (c == null) {
+
+                    return;
+
+                }
+
+            }
+
+            JFrame myFrame = (JFrame)c;
+
+            MyPanel myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
+
+            Insets myInsets = myFrame.getInsets();
+
+            int x1 = myInsets.left;
+
+            int y1 = myInsets.top;
+
+            e.translatePoint(-x1, -y1);
+
+            int x = e.getX();
+
+            int y = e.getY();
+
+            myPanel.x = x;
+
+            myPanel.y = y;
+
+            int gridX = myPanel.getGridX(x, y);
+
+            int gridY = myPanel.getGridY(x, y);
+            
+            
+          
+
+       
+             if ((myPanel.mouseDownGridX == -1) || (myPanel.mouseDownGridY == -1)) {
+
+                //Had pressed outside
+
+                //Do nothing
+
+            } else {
+
+                if ((gridX == -1) || (gridY == -1)) {
+
+                    //Is releasing outside
+
+                    //Do nothing
+
+                } else {
+
+                    if ((myPanel.mouseDownGridX != gridX) || (myPanel.mouseDownGridY != gridY)) {
+
+                        //Released the mouse button on a different cell where it was pressed
+
+                        //Do nothing
+
+                    }
+
+                        //Released the mouse button on the same cell where it was pressed
+
+                   else {             
+
+                        //On the grid other than on the left column and on the top row:
+                    	System.out.println("Number of mines neightbours: " + 	myPanel.minesSurroundingOurSquare[myPanel.mouseDownGridX][myPanel.mouseDownGridY] );
+                        myPanel.minesUncovered[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = true;
+                        
+                        if(myPanel.minesUncovered[0][0]){
+                        	myPanel.resetGame();
+                        	myPanel.minesUncovered[0][0] = false;
+                        }
+                         
+                    }
+
+                }
+
+            }
+
+            myPanel.repaint();
+
+            break;
+
+        case 3:    //Right mouse button
+
+//           // Do nothing
+            Component c2 = e.getComponent();
+
+            while (!(c2 instanceof JFrame)) {
+
+                c2 = c2.getParent();
+
+                if (c2 == null) {
+
+                    return;
+
+                }
+
+            }
+            JFrame myFrame2 = (JFrame) c2;
+            MyPanel myPanel2 = (MyPanel) myFrame2.getContentPane().getComponent(0);
+            Insets myInsets2 = myFrame2.getInsets();
+            int x3 = myInsets2.left;
+            int y3 = myInsets2.top;
+            e.translatePoint(-x3, -y3);
+            int x2 = e.getX();
+            int y2 = e.getY();
+            myPanel2.x = x2;
+            myPanel2.y = y2;
+           
+            myPanel2.mouseDownGridX = myPanel2.getGridX(x2, y2);
+            myPanel2.mouseDownGridY = myPanel2.getGridY(x2, y2);
+
+            int gridX2 = myPanel2.getGridX(x2, y2);
+            int gridY2 = myPanel2.getGridY(x2, y2);
+            
+            
+            
+           
+//            if ((gridX2 == -1) || (gridY2 == -1)) {    // Clicked outside
+//
+//            }
+//
+//
+//            break;
+        	//Right mouse button
+			if ((myPanel2.mouseDownGridX == -1) || (myPanel2.mouseDownGridY == -1)) {
+				//Had pressed outside
+				//Do nothing
+			} else {
+				if ((gridX2 == -1) || (gridY2 == -1)) {
+					//Is releasing outside
+					//Do nothing
+				} else {
+					if ((myPanel2.mouseDownGridX != gridX2) || (myPanel2.mouseDownGridY != gridY2)) {
+						//Released the mouse button on a different cell where it was pressed
+						//Do nothing
+					} else {
+						//Released the mouse button on the same cell where it was pressed
+						Color newColor = null;
+						newColor = Color.RED;
+						myPanel2.colorArray[myPanel2.mouseDownGridX][myPanel2.mouseDownGridY] = newColor;
+						myPanel2.repaint();
+					}
+				}
+			}
+			break;
+        	
+
+        default:    //Some other button (2 = Middle mouse button, etc.)
+
+            //Do nothing
+
+            break;
+
+        }
+
+    }
+
+}
---
+++ Lab5/src/MyPanel.java
@@ -0,0 +1,287 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Insets;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class MyPanel extends JPanel {
+	private static final long serialVersionUID = 3426940946811133635L;
+	private static final int GRID_X = 25;
+	private static final int GRID_Y = 25;
+	private static final int INNER_CELL_SIZE = 29;
+	private static final int TOTAL_COLUMNS = 10;
+	private static final int TOTAL_ROWS = 11;   
+	public int x = -1;
+	public int y = -1;
+	public int mouseDownGridX = 0;
+	public int mouseDownGridY = 0;	
+	public boolean gameOver = false;
+	public  Color[][] colorArray = new Color[TOTAL_COLUMNS][TOTAL_ROWS];
+	public int[][] hiddenMines = new int [TOTAL_COLUMNS][TOTAL_ROWS];
+	public int[][] minesSurroundingOurSquare = new int [TOTAL_COLUMNS][TOTAL_ROWS];
+	public boolean[][] minesUncovered = new boolean[TOTAL_COLUMNS][TOTAL_ROWS];
+	
+	Random random = new Random();
+	int surroundingMines;
+	
+	
+	public MyPanel() {   //This is the constructor... this code runs first to initialize
+		if (INNER_CELL_SIZE + (new Random()).nextInt(1) < 1) {	//Use of "random" to prevent unwanted Eclipse warning
+			throw new RuntimeException("INNER_CELL_SIZE must be positive!");
+		}
+		if (TOTAL_COLUMNS + (new Random()).nextInt(1) < 2) {	//Use of "random" to prevent unwanted Eclipse warning
+			throw new RuntimeException("TOTAL_COLUMNS must be at least 2!");
+		}
+		if (TOTAL_ROWS + (new Random()).nextInt(1) < 3) {	//Use of "random" to prevent unwanted Eclipse warning
+			throw new RuntimeException("TOTAL_ROWS must be at least 3!");
+		}
+		for (int x = 0; x < TOTAL_COLUMNS; x++) {   //Top row
+			colorArray[x][0] = Color.LIGHT_GRAY;
+		//	colorArray[x][0] = Color.BLACK;
+		}
+		for (int y = 0; y < TOTAL_ROWS; y++) {   //Left column
+			colorArray[0][y] = Color.LIGHT_GRAY;
+		}
+		for (int x = 1; x < TOTAL_COLUMNS; x++) {   //The rest of the grid
+			for (int y = 1; y < TOTAL_ROWS; y++) {
+				colorArray[x][y] = Color.WHITE;
+			}
+			
+		}
+	
+	
+		for (int x = 1; x < TOTAL_COLUMNS; x++) {   
+			for (int y = 1; y < TOTAL_ROWS - 1; y++) {
+				if(random.nextInt(30) < 6){
+					hiddenMines[x][y] = 1;               
+				}
+				else{
+					hiddenMines[x][y] = 0;
+					
+				}
+				minesUncovered[x][y] = false;            
+			}
+			}
+			
+		for (int x = 1; x < TOTAL_COLUMNS; x++) {          
+			for (int y = 1; y < TOTAL_ROWS - 1; y++) {
+				surroundingMines = 0;
+				for (int m = 1; m < TOTAL_COLUMNS; m++) {          
+					for (int n = 1; n < TOTAL_ROWS - 1; n++) {
+						if(!(x == m &&  y == n)){
+							if(isNeightbor(x, y, m, n)){
+								surroundingMines++;
+						
+						}
+			         }
+                 }
+					
+					minesSurroundingOurSquare[x][y] = surroundingMines;
+			
+				}
+			}			
+		}
+	}
+		
+	
+
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		//Compute interior coordinates
+		Insets myInsets = getInsets();
+		int x1 = myInsets.left;
+		int y1 = myInsets.top;
+		int x2 = getWidth() - myInsets.right - 1;
+		int y2 = getHeight() - myInsets.bottom - 1;
+		int width = x2 - x1;
+		int height = y2 - y1;
+
+		//Paint the background
+		g.setColor(Color.LIGHT_GRAY);
+		g.fillRect(x1, y1, width + 1, height + 1);
+
+		//Draw the grid minus the bottom row (which has only one cell)
+		//By default, the grid will be 10x10 (see above: TOTAL_COLUMNS and TOTAL_ROWS) 
+		g.setColor(Color.BLACK);
+		for (int y = 0; y <= TOTAL_ROWS - 1; y++) {
+			g.drawLine(x1 + GRID_X, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)), x1 + GRID_X + ((INNER_CELL_SIZE + 1) * TOTAL_COLUMNS), y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)));
+		}
+		for (int x = 0; x <= TOTAL_COLUMNS; x++) {
+			g.drawLine(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y, x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)), y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS - 1)));
+		}
+
+		//Draw an additional cell at the bottom left
+		//g.drawRect(x1 + GRID_X, y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS - 1)), INNER_CELL_SIZE + 1, INNER_CELL_SIZE + 1);
+
+		//Paint cell colors
+		for (int x = 0; x < TOTAL_COLUMNS; x++) {
+			for (int y = 0; y < TOTAL_ROWS; y++) {
+				if ((x == 0) || (y != TOTAL_ROWS - 1)) {
+					Color c = colorArray[x][y];
+					g.setColor(c);
+					g.fillRect(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 1, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)) + 1, INNER_CELL_SIZE, INNER_CELL_SIZE);
+				}
+				
+				if(minesUncovered[x][y] == true){
+					if(hiddenMines[x][y] == 0){
+						g.setColor(Color.GRAY);
+						g.fillRect(x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 1, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)) + 1, INNER_CELL_SIZE, INNER_CELL_SIZE);
+					}
+					else{
+						g.setColor(Color.BLACK);
+						for (int i = 0; i < TOTAL_COLUMNS; i++) {
+							for (int j = 0; j < TOTAL_ROWS; j++) {
+								if(hiddenMines[i][j]==1){
+									minesUncovered[i][j]=true;
+									g.fillRect(x1 + GRID_X + (i * (INNER_CELL_SIZE + 1)) + 1, y1 + GRID_Y + (j * (INNER_CELL_SIZE + 1)) + 1, INNER_CELL_SIZE, INNER_CELL_SIZE);
+									}
+								}
+							}
+					    }
+					
+				}
+				if(minesUncovered[x][y] == true){
+				
+					if(hiddenMines[x][y] == 0 && minesSurroundingOurSquare[x][y] != 0){
+						if(minesSurroundingOurSquare[x][y] == 1){
+							g.setColor(Color.BLUE);
+						}
+						else if(minesSurroundingOurSquare[x][y] == 2){
+							g.setColor(Color.GREEN);
+						}
+						else if(minesSurroundingOurSquare[x][y] == 3){
+							g.setColor(Color.RED);
+						}
+						else if(minesSurroundingOurSquare[x][y] == 4){
+							g.setColor(Color.DARK_GRAY);
+						}
+						else if(minesSurroundingOurSquare[x][y] == 5){
+							g.setColor(Color.PINK);
+						}
+						else if(minesSurroundingOurSquare[x][y] == 6){
+							g.setColor(Color.CYAN);
+						}
+					     g.setFont(new Font("Times New Roman", Font.BOLD, 20));
+					     g.drawString(Integer.toString(minesSurroundingOurSquare[x][y]), x1 + GRID_X + (x * (INNER_CELL_SIZE + 1)) + 5, y1 + GRID_Y + (y * (INNER_CELL_SIZE + 1)) + 20);
+					}
+				}
+			}
+		}
+		
+		if(gameOver == true){
+			JOptionPane.showMessageDialog(null, "GAME OVER!");
+			System.exit(0);
+		}
+	
+	}
+
+	public int getGridX(int x, int y) {
+		Insets myInsets = getInsets();
+		int x1 = myInsets.left;
+		int y1 = myInsets.top;
+		x = x - x1 - GRID_X;
+		y = y - y1 - GRID_Y;
+		if (x < 0) {   //To the left of the grid
+			return -1;
+		}
+		if (y < 0) {   //Above the grid
+			return -1;
+		}
+		if ((x % (INNER_CELL_SIZE + 1) == 0) || (y % (INNER_CELL_SIZE + 1) == 0)) {   //Coordinate is at an edge; not inside a cell
+			return -1;
+		}
+		x = x / (INNER_CELL_SIZE + 1);
+		y = y / (INNER_CELL_SIZE + 1);
+		if (x == 0 && y == TOTAL_ROWS - 1) {    //The lower left extra cell
+			return x;
+		}
+		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
+			return -1;
+		}
+		return x;
+	}
+	public int getGridY(int x, int y) {
+		Insets myInsets = getInsets();
+		int x1 = myInsets.left;
+		int y1 = myInsets.top;
+		x = x - x1 - GRID_X;
+		y = y - y1 - GRID_Y;
+		if (x < 0) {   //To the left of the grid
+			return -1;
+		}
+		if (y < 0) {   //Above the grid
+			return -1;
+		}
+		if ((x % (INNER_CELL_SIZE + 1) == 0) || (y % (INNER_CELL_SIZE + 1) == 0)) {   //Coordinate is at an edge; not inside a cell
+			return -1;
+		}
+		x = x / (INNER_CELL_SIZE + 1);
+		y = y / (INNER_CELL_SIZE + 1);
+		if (x == 0 && y == TOTAL_ROWS - 1) {    //The lower left extra cell
+			return y;
+		}
+		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
+			return -1;
+		}
+		return y;
+	}
+	public static int getTotalColumns() {
+		return TOTAL_COLUMNS;
+	}
+	public static int getTotalRows() {
+		return TOTAL_ROWS;
+	}
+	public boolean isNeightbor(int squareX, int squareY, int neightborX, int neightborY ){
+		if(squareX - neightborX < 2 && squareX - neightborX > -2 && squareY - neightborY < 2 && squareY - neightborY > -2 && hiddenMines[neightborX][neightborY] == 1){
+			return true;
+		}
+		return false;
+		
+	}
+	public void resetGame(){
+		
+		for (int x = 1; x < TOTAL_COLUMNS; x++) {   //Generate random mines with 20% of probability for each square.
+			for (int y = 1; y < TOTAL_ROWS - 1; y++) {
+				if(random.nextInt(100) < 20){
+					hiddenMines[x][y] = 1;                // If it is 1, it has a mine. If it is 0, it is empty.
+				}
+				else{
+					hiddenMines[x][y] = 0;
+					
+				}
+				minesUncovered[x][y] = false;            // Initially, all mines will be covered.
+			}
+		}
+		
+			
+		
+			
+		for (int x = 1; x < TOTAL_COLUMNS; x++) {          
+			for (int y = 1; y < TOTAL_ROWS - 1; y++) {
+				surroundingMines = 0;
+				for (int m = 1; m < TOTAL_COLUMNS; m++) {          
+					for (int n = 1; n < TOTAL_ROWS - 1; n++) {
+						if(!(x == m &&  y == n)){
+							if(isNeightbor(x, y, m, n)){
+								surroundingMines++;
+						
+						}
+			         }
+                 }
+					
+					minesSurroundingOurSquare[x][y] = surroundingMines;
+			
+				}
+				}
+			}
+		
+		
+	}
+	
+	
+}
